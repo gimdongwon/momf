84897e8087e8e1bc4343ec8926d94dd531893f79
kimdongwon <ehddnjs8989@gmail.com>
2018-08-25 21:06:26 +0900

    helmet, font-family clear

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Helmet } from "react-helmet";
 import "./App.css";
 import Homepage from "./components/Homepage";
 
@@ -6,6 +7,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>MOMF</title>
+          <link rel="canonical" href="http://mysite.com/example" />
+        </Helmet>
         <Homepage />
       </div>
     );
