b647146405bdbf65044a4662fe69252ef689940b
kimdongwon <ehddnjs8989@gmail.com>
2018-12-11 17:01:17 +0900

    Complete setting

--- src/components/pages/People.js
+++ src/components/pages/People.js
@@ -1,12 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
 export default class People extends React.Component {
   render() {
     return (
-      <React.Fragment>
-        <h1>Hi People!</h1>
-        <Link to="/">home</Link>
-      </React.Fragment>
+      <section className="content-wrapper">
+        <header className="contents-header">
+          <h1>사람들</h1>
+        </header>
+        <section className="content-box contents-life">
+          <header>
+            <h2>MOMF의 사람들</h2>
+          </header>
+          <section className="card">
+            <h3>2011</h3>
+            <p>동원 희준</p>
+          </section>
+          <div className="comment">
+            <p>질문이 있으시다면 카카오 플러스 친구를 등록해주세요</p>
+          </div>
+        </section>
+      </section>
     );
   }
 }
