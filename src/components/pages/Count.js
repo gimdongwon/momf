fe5c881a47282a0d76c205f1753d0109517144c0
kimdongwon <ehddnjs8989@gmail.com>
2019-08-07 20:55:53 +0900

    add_count_finish_func

--- src/components/pages/Count.js
+++ src/components/pages/Count.js
@@ -9,11 +9,29 @@ export default class Count extends React.Component {
     minutes: null,
     hours: null,
     days: null,
-    seconds: null
+    seconds: null,
+    gap: 1801,
+    timerFinish: false
   };
+  repeat = () => {
+    let timer = setInterval(() => {
+      this.method();
+      console.log(this.state.gap);
+      if (this.state.gap < 1800) {
+        console.log("clear");
+        clearInterval(timer);
+        this.setState({ timerFinish: true });
+      }
+    }, 1000);
+  };
+
   method = async () => {
-    const dDay = await new Date("2019/10/3");
-    dDay.setHours(16);
+    // const dDay = await new Date("2019/10/3");
+    // dDay.setHours(16);
+    const dDay = await new Date("2019/8/7");
+    dDay.setHours(18);
+    dDay.setMinutes(21);
+
     let now = await new Date();
     let gap = dDay - now;
 
@@ -23,15 +41,17 @@ export default class Count extends React.Component {
     let seconds = Math.floor(
       gap / 1000 - 24 * 60 * 60 * days - 60 * 60 * hours - 60 * minutes
     );
+    console.log(gap);
     this.setState({
       days,
       hours,
       minutes,
-      seconds
+      seconds,
+      gap
     });
   };
   componentDidMount() {
-    setInterval(this.method, 1000);
+    this.repeat();
   }
   render() {
     return (
@@ -50,24 +70,30 @@ export default class Count extends React.Component {
                 <span className="accent">Is Coming.</span>
               </p>
             </div>
-            <div className="time_wrap">
-              <div className="wrap">
-                <div className="days value">{this.state.days}</div>
-                <div className="days_title">days</div>
+            {this.state.timerFinish ? (
+              <h1 className="backTitle">MOMF is back!</h1>
+            ) : (
+              <div className="time_wrap">
+                <div className="wrap">
+                  <div className="days value">{this.state.days}</div>
+                  <div className="days_title">days</div>
+                </div>
+                <div className="wrap">
+                  <div className="hours value">{this.state.hours}</div>
+                  <div className="hours_title">hours</div>
+                </div>
+                <div className="wrap">
+                  <div className="minutes value">{this.state.minutes}</div>
+                  <div className="minutes_title">minutes</div>
+                </div>
+                <div className="wrap">
+                  <div className="seconds value accent">
+                    {this.state.seconds}
+                  </div>
+                  <div className="seconds_title">seconds</div>
+                </div>
               </div>
-              <div className="wrap">
-                <div className="hours value">{this.state.hours}</div>
-                <div className="hours_title">hours</div>
-              </div>
-              <div className="wrap">
-                <div className="minutes value">{this.state.minutes}</div>
-                <div className="minutes_title">minutes</div>
-              </div>
-              <div className="wrap">
-                <div className="seconds value accent">{this.state.seconds}</div>
-                <div className="seconds_title">seconds</div>
-              </div>
-            </div>
+            )}
           </section>
           <KakaoFriend />
         </section>
